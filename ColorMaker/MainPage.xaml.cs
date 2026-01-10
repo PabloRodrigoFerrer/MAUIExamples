ac755baa54b6f695f02939b6f7270d35215929bb
PabloRodrigoFerrer <rferrer42@gmail.com>
2026-01-10 16:42:23 -0300

    Init and complete app

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -1,24 +1,69 @@
-﻿namespace ColorMaker
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace ColorMaker
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
+        private bool isRandom = false;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            count++;
+            if (!isRandom)
+            {
+                var red = sldRed.Value;
+                var blue = sldBlue.Value;
+                var green = sldGreen.Value;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                Color color = Color.FromRgb(red, green, blue);
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                setColor(color);
+            }
+        }
+
+        private void setColor(Color color)
+        {
+            Container.BackgroundColor = color;
+            btnRandom.BackgroundColor = color;
+
+            lblHex.Text = color.ToHex();
+            lblMainTitle.TextColor = color;
+
+            sldRed.ThumbColor = color;
+            sldBlue.ThumbColor = color;
+            sldGreen.ThumbColor = color;
+        }
+
+        private void btnRandom_Clicked(object sender, EventArgs e)
+        {
+            isRandom = true;
+            var random = new Random();
+
+            var color = Color.FromRgb(
+                random.Next(0, 256),
+                random.Next(0, 256),
+                random.Next(0, 256)
+                );
+
+            setColor(color);
+
+            sldRed.Value = color.Red;
+            sldBlue.Value = color.Blue;
+            sldGreen.Value = color.Green;
+
+            isRandom = false;
+        }
+
+        private async void btnCopy_Clicked(object sender, EventArgs e)
+        {
+            await Clipboard.SetTextAsync(lblHex.Text);
+            var toast = Toast.Make("Color code copied to clipboard!", CommunityToolkit.Maui.Core.ToastDuration.Short, 12);
+            await toast.Show();
         }
     }
 }
